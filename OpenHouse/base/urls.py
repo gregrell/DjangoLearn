c251ffb58733122b53d8423ee8d6b9ba8d441e54
Greg Rell <gregory.rell@universalorlando.com>
2022-07-21 15:28:02 -0400

    Added urls.py as a routing handler in the BASE app, and also adjusted the routing file urls.py in the OpenHouse main app to use an include function so as to route to the base app routing

---
+++ OpenHouse/base/urls.py
@@ -0,0 +1,11 @@
+"""Created this file to manage url routing within the BASE app itself, and not the OpenHouse Django
+    Full project"""
+
+
+from django.urls import path
+from . import views #the . notation here indicates we are importing from the same folder as this file
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/', views.room, name='room')
+]
--- OpenHouse/base/views.py
+++ OpenHouse/base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+"""Rell added logic from tutorial, created a new function call home taking in the request object, returning 
+    an HTTP Response Object"""
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+
+def room(request):
+    return HttpResponse('Room')
