7577922de2576d046f6cbf7a824a5519a39c48b4
Greg Rell <gregory.rell@universalorlando.com>
2022-07-22 13:49:52 -0400

    Did initial migrations for django, created a room model in the base/models.py, registered that model with the admin site in admin.py

--- OpenHouse/base/admin.py
+++ OpenHouse/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Room
+
+
+admin.site.register(Room)  # This will register the model to be used in the admin page so we can interact with the
+# data (from the admin page)
+
--- OpenHouse/base/models.py
+++ OpenHouse/base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)  # the null input arguments allow the database to have a
+    # blank value on creation, and on saving
+    # participants =
+    updated = models.DateTimeField(auto_now=True)  # Any time the save method is called, this will auto populate
+    created = models.DateTimeField(auto_now_add=True)  # The difference between auto_now and auto_add_now is that
+
+    # auto_now will take a timestampe every time, and auto_now_add will only do a timestamp when the instance is
+    # created ie. only the creation timestamp
+    def __str__(self):
+        return self.name
+
